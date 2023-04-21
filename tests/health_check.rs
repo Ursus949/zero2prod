fc92edef1510784fc95b2682b6ff9aa35c3a31ca
ursus949 <18504481+Ursus949@users.noreply.github.com>
2023-04-21 09:45:32 -0500

    first integration test working

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,12 @@
 //! tests/health_check.rs
 
-use zero2prod::main;
-
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn our app.");
+    spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:80000/health_check")
+        .get("http://127.0.0.1:8000/health_check")
         .send()
         .await
         .expect("Failed to execture request.");
@@ -17,6 +15,7 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> Result<(), std::io::Error> {
-    zero2prod::run().await
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
 }
