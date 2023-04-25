458c3680691f0e596919455da82933eea40e5a12
ursus949 <18504481+Ursus949@users.noreply.github.com>
2023-04-25 11:59:57 -0500

    fixed clippy suggestion

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(&configuration.database.connection_string().expose_secret())
+    let connection = PgPool::connect(configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
