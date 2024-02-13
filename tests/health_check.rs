d99f66a5c4165caaa60d30bc14eb3813b4abd763
Ursus949 <18504481+Ursus949@users.noreply.github.com>
2024-02-13 14:11:32 -0600

    health_check/tests (3.6)

---
+++ tests/health_check.rs
@@ -0,0 +1,27 @@
+use std::net::TcpListener;
+use zero2prod::run;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failedtobind randomport");
+
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failedtobind address");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
